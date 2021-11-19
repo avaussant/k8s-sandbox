5f0ea1af68b2bafdc7236addef41838223f897da
Alexandre Vaussant <avaussant@axway.com>
2021-11-19 22:48:03 +0100

    add exo4

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# k8s-sandbox
+# k8s-sandbox - How to play with Kubernetes for learning
 How to play with Kubernetes + debugging
+
+## Prerequisites tools
+
+- docker - https://docs.docker.com/install/linux/docker-ce/binaries/
+- Prefered - MacOs or Linux (not tested on wsl)
+
+## Usage
+
+```bash
+$ git clone https://github.com/avaussant/k8s-sandbox.git
+$ cd k8s-sandbox
+$ ./launch
+```
+
+## Tests
+
+1. _**Ingresses**_
+
+<details><summary>lab --exo1</summary>
+
+- How to debug an ingress, what can be wrong ?
+    - 2 services
+    - 2 pods
+    - 2 urls
+    - 1 error
+
+Try to find the issue fix it and relaunch the script 
+</details>
+
+
+```console
+[ OUT ] ====================================================
+[ OUT ]  DATE : 2020-02-11_11:14:32
+[ OUT ]  Repo elastic existing in your helm config
+[ OUT ] ====================================================
+[INFOS]
+[INFOS] ====================================================
+[INFOS]  DATE : 2020-02-11_11:14:32
+[INFOS]  Starting elasticsearch deployment
+```
+
+## How to update ELK configs
+
+1. For ealsticsearch - Go to [es-values](elasticsearch/values.yaml)
+
+2. For kibana  - Go to [es-values](kibana/values.yaml)
+
+After modification you can just use the command **aps-elk.sh -u**
--- scripts/lab.sh
+++ scripts/lab.sh
@@ -6,39 +6,17 @@ source $(dirname 0)/scripts/libs.bash
 export ACTION=$1
 export K8S_VERSION="1.21.1"
 
-update_master_endpoint() {
-  msg_info "Modifying Kubernetes config to point to Kind master node"
-  MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' k8s-sandbox-control-plane)
-  sed -i "s/^    server:.*/    server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/config
 
-  chmod -R go-r $HOME/.kube/config
-
-  msg_info "Attach client to kind network"
-  docker network connect kind k8s-client
-
-  msg_output "Add helm repo"
-  helm repo add stable https://charts.helm.sh/stable
-  helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
-  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-  helm repo update
-}
-
-lab_up() {
-  msg_info "Create k8s Lab"
-  kind create cluster --config config/kind.yml --name k8s-sandbox --image kindest/node:v${K8S_VERSION}
-
-  update_master_endpoint
-
-  msg_output "$(printf %"s\n" && kubectl cluster-info --context kind-k8s-sandbox)"
-
+nginx_ingress_controller() {
   msg_info "Install Nginx Ingress Controller"
   kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
   kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
+}
 
+gatekeeper_opa() {
   msg_info "Install Gatekeeper/Opa"
   helm upgrade -i gatekeeper gatekeeper/gatekeeper \
     --version=3.7.0 \
@@ -63,6 +41,39 @@ lab_up() {
     -n gatekeeper-system
 
   sleep 5
+}
+
+update_master_endpoint() {
+  msg_info "Modifying Kubernetes config to point to Kind master node"
+  MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' k8s-sandbox-control-plane)
+  sed -i "s/^    server:.*/    server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/config
+
+  chmod -R go-r $HOME/.kube/config
+
+  msg_info "Attach client to kind network"
+  docker network connect kind k8s-client
+
+  msg_output "Add helm repo"
+  helm repo add stable https://charts.helm.sh/stable
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  helm repo add elastic https://helm.elastic.co
+  helm repo update
+}
+
+lab_up() {
+  msg_info "Create k8s Lab"
+  kind create cluster --config config/kind.yml --name k8s-sandbox --image kindest/node:v${K8S_VERSION}
+
+  update_master_endpoint
+
+  msg_output "$(printf %"s\n" && kubectl cluster-info --context kind-k8s-sandbox)"
+
+  nginx_ingress_controller
+
+  gatekeeper_opa
+
   lab_ready
 }
 
@@ -139,16 +150,17 @@ test3 (){
 }
 
 test4 (){
-  msg_info "Create k8s Lab for Monitoring and Logging - waiting ............"
+  msg_info "Create k8s Lab for Monitoring - waiting ............"
   if ! kind create cluster --config config/kind.yml --name k8s-sandbox --image kindest/node:v${K8S_VERSION} > /dev/null 2>&1; then
     msg_info "It might be hard to put all in the same cluster :-("
     msg_error "Please delete old lab before and try again with a fresh installation"
   else
     update_master_endpoint
     msg_output "$(printf %"s\n" && kubectl cluster-info --context kind-k8s-sandbox)"
+    nginx_ingress_controller
   fi
 
-  msg_info "Exercice for Monitoring and Logging"
+  msg_info "Exercice for Monitoring"
   msg_info "Install Prometheus Adapter"
   helm upgrade -i prometheus-adapter prometheus-community/prometheus-adapter \
     -f tests/exo-4/prometheus-adapter.yaml \
@@ -164,7 +176,41 @@ test4 (){
     --atomic \
     --cleanup-on-fail \
     -n monitoring --create-namespace
+
+  msg_info "Install DB for my APP"
+  helm upgrade -i my-db tests/exo-4/postgresql \
+    -f tests/exo-4/postgresql/pg-values.yaml \
+    --atomic \
+    --cleanup-on-fail \
+    --namespace exo4  \
+    --create-namespace
+
+  kubectl wait --namespace exo4 \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/name=my-db \
+    --timeout=90s
+
+  msg_info "Install DB metrics exporter for my APP + Rules for Prometheus"
+  helm upgrade -i  postgres-exporter tests/exo-4/prometheus-postgres-exporter \
+    --atomic \
+    --cleanup-on-fail \
+    --namespace exo4 \
+    --create-namespace \
+    -f tests/exo-4/prometheus-postgres-exporter/pg-exporter-values.yaml \
+    -f tests/exo-4/prometheus-postgres-exporter/prom-pg-rules.yaml
+
+  msg_info "Ingress for Prometheus"
+  kubectl apply -f tests/exo-4/prometheus-ingress.yaml
+  
+  msg_output "1 - try from your browser : localhost"
+  msg_output "2 - Look the rules in place"
+  msg_output "3 - Create Custom rules with metrics coming from pgsql exporter"
+  msg_output "4 - If possible crash the db to get firing status with your Rule"
+  msg_info "Indication - You can take a look on the file tests/exo-4/prometheus-postgres-exporter/prom-pg-rules.yaml"
+  msg_info "Indication - for direct access to db try to follow the NOTE.txt"
+
 }
+  # msg_info "Install small elastic deployment"
 
 ##########################################
 # Main function
